837d9a036b557e1f18176b8bb73c3d69ad23b815
Sean Batzel <romulus108@protonmail.com>
2022-01-06 15:47:10 -0500

    Benchmarks and tests for pallets/trust.

--- build.sh
+++ build.sh
@@ -1,3 +1,13 @@
+rustup update nightly
+rustup update stable
+rustup default nightly
+rustup target add wasm32-unknown-unknown --toolchain nightly
+
+cargo build
+cargo test
+cargo bench
+cargo doc
+
 cargo build --release
 cargo doc --release
 cp -rf ./target/doc ./docs
