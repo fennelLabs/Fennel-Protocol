42c7aeb4b83aad39bccab01bdd7a4ac21aad9cfa
Husky <huskydev@pm.me>
2022-01-07 18:34:07 -0500

    adding more tests

--- pallets/identity/src/tests.rs
+++ pallets/identity/src/tests.rs
@@ -3,9 +3,41 @@ use frame_support::{assert_ok};
 
 #[test]
 fn issue_identity() {
-	new_test_ext().execute_with(|| {
-		assert_ok!(IdentityModule::create_identity(Origin::signed(1)));
-		assert_eq!(IdentityModule::identity_list(1), Some(0));
-		assert_eq!(IdentityModule::identity_number(), Some(1));
-	});
+    new_test_ext().execute_with(|| {
+        assert_ok!(IdentityModule::create_identity(Origin::signed(1)));
+    });
+}
+
+#[test]
+fn issue_identity_increments_by_number_of_times_called() {
+    new_test_ext().execute_with(|| {
+        assert_ok!(IdentityModule::create_identity(Origin::signed(1)));
+        assert_ok!(IdentityModule::create_identity(Origin::signed(2)));
+        assert_ok!(IdentityModule::create_identity(Origin::signed(3)));
+
+        assert_eq!(IdentityModule::identity_number(), 3);
+    });
+}
+
+#[test]
+fn issue_identity_registers_different_account_ids_with_new_identities() {
+    new_test_ext().execute_with(|| {
+        assert_ok!(IdentityModule::create_identity(Origin::signed(300)));
+        assert_ok!(IdentityModule::create_identity(Origin::signed(200)));
+
+        assert_eq!(IdentityModule::identity_list(300), Some(1));
+        assert_eq!(IdentityModule::identity_list(200), Some(2));
+    });
+}
+
+#[test]
+fn issue_identity_registers_same_account_id_with_multiple_new_identities() {
+    new_test_ext().execute_with(|| {
+        assert_ok!(IdentityModule::create_identity(Origin::signed(300)));
+        assert_ok!(IdentityModule::create_identity(Origin::signed(300)));
+
+        // this is where it begins to not make sense
+        assert_eq!(IdentityModule::identity_list(300), Some(1));
+        assert_eq!(IdentityModule::identity_list(300), Some(2));
+    });
 }
