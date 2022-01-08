c57ed555d06c5841d0ef9fd8f288c5d7cea53998
Husky <huskydev@pm.me>
2022-01-08 16:59:12 -0500

    adding tests

--- pallets/identity/src/tests.rs
+++ pallets/identity/src/tests.rs
@@ -1,5 +1,5 @@
-use crate::{mock::*};
-use frame_support::{assert_ok};
+use crate::{Error, mock::*};
+use frame_support::{assert_ok, assert_noop};
 
 #[test]
 fn issue_identity() {
@@ -38,5 +38,45 @@ fn issue_identity_registers_same_account_id_with_multiple_new_identities() {
 
         assert_eq!(IdentityModule::identity_list(300).contains(&1), true);
         assert_eq!(IdentityModule::identity_list(300).contains(&2), true);
+    });
+}
+
+#[test]
+fn revoke_identity() {
+    new_test_ext().execute_with(|| {
+        assert_ok!(IdentityModule::create_identity(Origin::signed(300)));
+        assert_ok!(IdentityModule::revoke_identity(Origin::signed(300), 1));
+    });
+}
+
+#[test]
+fn revoke_identity_multiple_from_different_accounts() {
+    new_test_ext().execute_with(|| {
+        assert_ok!(IdentityModule::create_identity(Origin::signed(300)));
+        assert_ok!(IdentityModule::create_identity(Origin::signed(200)));
+        
+        assert_ok!(IdentityModule::revoke_identity(Origin::signed(300), 1));
+        assert_ok!(IdentityModule::revoke_identity(Origin::signed(200), 2));
+    });
+}
+
+#[test]
+fn revoke_identity_multiple_from_same_account() {
+    new_test_ext().execute_with(|| {
+        assert_ok!(IdentityModule::create_identity(Origin::signed(300)));
+        assert_ok!(IdentityModule::create_identity(Origin::signed(300)));
+        
+        assert_ok!(IdentityModule::revoke_identity(Origin::signed(300), 2));
+        assert_ok!(IdentityModule::revoke_identity(Origin::signed(300), 1));
+    });
+}
+
+#[test]
+fn revoke_identity_from_non_owning_account() {
+    new_test_ext().execute_with(|| {
+        assert_noop!(
+            IdentityModule::revoke_identity(Origin::signed(300), 1),
+            Error::<Test>::IdentityNotOwned
+        );
     });
 }
