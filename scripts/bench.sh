44d534ddea1add2d50c4e576a2a639093208a0d5
Sean Batzel <romulus108@protonmail.com>
2022-01-11 13:27:13 -0500

    Further benchmarking improvements.

--- scripts/bench.sh
+++ scripts/bench.sh
@@ -1,2 +1,11 @@
-cargo build --features runtime-benchmarks
-./target/release/node-fennel-protocol benchmark --chain dev --execution wasm --wasm-execution compiled --pallet pallet_example --extrinsic '*' --steps 20 --repeat 10 --raw --output ./
+cargo build --release --features runtime-benchmarks
+echo "template"
+./target/release/node-fennel-protocol benchmark --chain dev --execution wasm --wasm-execution compiled --pallet pallet_template --extrinsic '*' --steps 20 --repeat 10 --raw --output ./
+echo "trust"
+./target/release/node-fennel-protocol benchmark --chain dev --execution wasm --wasm-execution compiled --pallet pallet_trust --extrinsic '*' --steps 20 --repeat 10 --raw --output ./
+echo "fennel-identity"
+./target/release/node-fennel-protocol benchmark --chain dev --execution wasm --wasm-execution compiled --pallet pallet_fennel_identity --extrinsic '*' --steps 20 --repeat 10 --raw --output ./
+echo "signal"
+./target/release/node-fennel-protocol benchmark --chain dev --execution wasm --wasm-execution compiled --pallet pallet_signal --extrinsic '*' --steps 20 --repeat 10 --raw --output ./
+echo "keystore"
+./target/release/node-fennel-protocol benchmark --chain dev --execution wasm --wasm-execution compiled --pallet pallet_keystore --extrinsic '*' --steps 20 --repeat 10 --raw --output ./
