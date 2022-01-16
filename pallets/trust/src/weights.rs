e7356ff8fdfd73b44dacdfe054be78affd8db788
Sean Batzel <romulus108@protonmail.com>
2022-01-16 17:21:52 -0500

    Weights are now correctly configured for testing.

--- pallets/trust/src/weights.rs
+++ pallets/trust/src/weights.rs
@@ -30,8 +30,7 @@
 #![cfg_attr(rustfmt, rustfmt_skip)]
 #![allow(unused_parens)]
 #![allow(unused_imports)]
-
-use frame_support::{traits::Get, weights::Weight};
+use frame_support::{traits::Get, weights::{Weight, constants::RocksDbWeight}};
 use core::marker::PhantomData;
 
 pub trait WeightInfo {
@@ -67,5 +66,32 @@ impl<T: frame_system::Config> WeightInfo for SubstrateWeights<T> {
 	fn remove_revoked_trust() -> Weight {
 		(7_599_000 as Weight)
 			.saturating_add(T::DbWeight::get().reads(1 as Weight))
+	}
+}
+
+impl WeightInfo for () {
+	// Storage: TrustModule TrustIssuance (r:1 w:1)
+	// Storage: TrustModule CurrentIssued (r:1 w:1)
+	fn issue_trust() -> Weight {
+		(33_807_000 as Weight)
+			.saturating_add(RocksDbWeight::get().reads(2 as Weight))
+			.saturating_add(RocksDbWeight::get().writes(2 as Weight))
+	}
+	// Storage: TrustModule TrustRevocation (r:1 w:1)
+	// Storage: TrustModule CurrentRevoked (r:1 w:1)
+	fn revoke_trust() -> Weight {
+		(33_776_000 as Weight)
+			.saturating_add(RocksDbWeight::get().reads(2 as Weight))
+			.saturating_add(RocksDbWeight::get().writes(2 as Weight))
+	}
+	// Storage: TrustModule TrustIssuance (r:1 w:0)
+	fn remove_trust() -> Weight {
+		(7_800_000 as Weight)
+			.saturating_add(RocksDbWeight::get().reads(1 as Weight))
+	}
+	// Storage: TrustModule TrustRevocation (r:1 w:0)
+	fn remove_revoked_trust() -> Weight {
+		(7_599_000 as Weight)
+			.saturating_add(RocksDbWeight::get().reads(1 as Weight))
 	}
 }
