d5ec9ea80931f5de4dc3306e09a20776a6489b27
Husky <huskydev@pm.me>
2022-01-17 19:55:34 -0500

    adding missing generated weights

--- pallets/identity/src/weights.rs
+++ pallets/identity/src/weights.rs
@@ -15,26 +15,40 @@ impl WeightInfo for () {
     // Storage: IdentityModule IdentityNumber (r:1 w:1)
     // Storage: IdentityModule IdentityList (r:1 w:1)
     fn create_identity() -> Weight {
-        (45_239_000 as Weight)
-            // Standard Error: 26_000
-            .saturating_add(DbWeight::get().reads(2 as Weight))
-            .saturating_add(DbWeight::get().writes(2 as Weight))
+        (47_568_000 as Weight)
+            .saturating_add(T::DbWeight::get().reads(2 as Weight))
+            .saturating_add(T::DbWeight::get().writes(2 as Weight))
     }
-
+    // Storage: IdentityModule RevokedIdentityNumber (r:1 w:1)
+    // Storage: IdentityModule IdentityList (r:1 w:1)
+    fn revoke_identity() -> Weight {
+        (43_296_000 as Weight)
+            // Standard Error: 6_000
+            .saturating_add(T::DbWeight::get().reads(2 as Weight))
+            .saturating_add(T::DbWeight::get().writes(2 as Weight))
+    }
     // Storage: IdentityModule IdentityList (r:1 w:0)
     // Storage: IdentityModule IdentityTraitList (r:1 w:1)
     fn add_or_update_identity_trait() -> Weight {
-        (49_785_000 as Weight)
-            // Standard Error: 38_000
-            .saturating_add(DbWeight::get().reads(2 as Weight))
-            .saturating_add(DbWeight::get().writes(1 as Weight))
+        (48_308_000 as Weight)
+            // Standard Error: 9_000
+            .saturating_add(T::DbWeight::get().reads(2 as Weight))
+            .saturating_add(T::DbWeight::get().writes(1 as Weight))
     }
-
     // Storage: IdentityModule IdentityList (r:1 w:0)
     // Storage: IdentityModule IdentityTraitList (r:0 w:1)
     fn remove_identity_trait() -> Weight {
-        (50_813_000 as Weight)
-            .saturating_add(DbWeight::get().reads(1 as Weight))
-            .saturating_add(DbWeight::get().writes(1 as Weight))
+        (48_195_000 as Weight)
+            .saturating_add(T::DbWeight::get().reads(1 as Weight))
+            .saturating_add(T::DbWeight::get().writes(1 as Weight))
+    }
+    // Storage: IdentityModule IdentityList (r:1 w:0)
+    // Storage: IdentityModule SignalCount (r:1 w:1)
+    // Storage: IdentityModule FennelSignal (r:0 w:1)
+    fn sign_for_identity() -> Weight {
+        (51_112_000 as Weight)
+            // Standard Error: 10_000
+            .saturating_add(T::DbWeight::get().reads(2 as Weight))
+            .saturating_add(T::DbWeight::get().writes(2 as Weight))
     }
 }
