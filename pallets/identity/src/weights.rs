3008840c9dc486a3e22bfb228b9a351bcc6d0d80
Husky <huskydev@pm.me>
2022-01-17 20:25:56 -0500

    using RocksDbWeight

--- pallets/identity/src/weights.rs
+++ pallets/identity/src/weights.rs
@@ -18,31 +18,31 @@ impl WeightInfo for () {
     // Storage: IdentityModule IdentityList (r:1 w:1)
     fn create_identity() -> Weight {
         (47_568_000 as Weight)
-            .saturating_add(T::DbWeight::get().reads(2 as Weight))
-            .saturating_add(T::DbWeight::get().writes(2 as Weight))
+            .saturating_add(RocksDbWeight::get().reads(2 as Weight))
+            .saturating_add(RocksDbWeight::get().writes(2 as Weight))
     }
     // Storage: IdentityModule RevokedIdentityNumber (r:1 w:1)
     // Storage: IdentityModule IdentityList (r:1 w:1)
     fn revoke_identity() -> Weight {
         (43_296_000 as Weight)
             // Standard Error: 6_000
-            .saturating_add(T::DbWeight::get().reads(2 as Weight))
-            .saturating_add(T::DbWeight::get().writes(2 as Weight))
+            .saturating_add(RocksDbWeight::get().reads(2 as Weight))
+            .saturating_add(RocksDbWeight::get().writes(2 as Weight))
     }
     // Storage: IdentityModule IdentityList (r:1 w:0)
     // Storage: IdentityModule IdentityTraitList (r:1 w:1)
     fn add_or_update_identity_trait() -> Weight {
         (48_308_000 as Weight)
             // Standard Error: 9_000
-            .saturating_add(T::DbWeight::get().reads(2 as Weight))
-            .saturating_add(T::DbWeight::get().writes(1 as Weight))
+            .saturating_add(RocksDbWeight::get().reads(2 as Weight))
+            .saturating_add(RocksDbWeight::get().writes(1 as Weight))
     }
     // Storage: IdentityModule IdentityList (r:1 w:0)
     // Storage: IdentityModule IdentityTraitList (r:0 w:1)
     fn remove_identity_trait() -> Weight {
         (48_195_000 as Weight)
-            .saturating_add(T::DbWeight::get().reads(1 as Weight))
-            .saturating_add(T::DbWeight::get().writes(1 as Weight))
+            .saturating_add(RocksDbWeight::get().reads(1 as Weight))
+            .saturating_add(RocksDbWeight::get().writes(1 as Weight))
     }
     // Storage: IdentityModule IdentityList (r:1 w:0)
     // Storage: IdentityModule SignalCount (r:1 w:1)
@@ -50,7 +50,7 @@ impl WeightInfo for () {
     fn sign_for_identity() -> Weight {
         (51_112_000 as Weight)
             // Standard Error: 10_000
-            .saturating_add(T::DbWeight::get().reads(2 as Weight))
-            .saturating_add(T::DbWeight::get().writes(2 as Weight))
+            .saturating_add(RocksDbWeight::get().reads(2 as Weight))
+            .saturating_add(RocksDbWeight::get().writes(2 as Weight))
     }
 }
