51ab07689200bfb12e5c33486ee600829c17b79f
Ed Hertzog <edhertzog@gmail.com>
2022-02-08 15:33:53 -0500

    Upgrade to parity-scale-codec

--- pallets/signal/src/lib.rs
+++ pallets/signal/src/lib.rs
@@ -6,7 +6,7 @@ pub use pallet::*;
 pub mod pallet {
     use frame_support::{dispatch::DispatchResult, inherent::Vec, pallet_prelude::*};
     use frame_system::pallet_prelude::*;
-
+    
     #[pallet::config]
     pub trait Config: frame_system::Config {
         type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
@@ -29,7 +29,6 @@ pub mod pallet {
     }
 
     #[pallet::error]
-    #[derive(scale_info::TypeInfo)]
     pub enum Error<T> {
         NoneValue,
         StorageOverflow,
