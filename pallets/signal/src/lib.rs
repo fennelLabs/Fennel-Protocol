314acec054baca031d93466f9c0bcbd13625484b
Ed Hertzog <edhertzog@gmail.com>
2022-02-08 15:45:54 -0500

    cargo fmt

--- pallets/signal/src/lib.rs
+++ pallets/signal/src/lib.rs
@@ -6,7 +6,7 @@ pub use pallet::*;
 pub mod pallet {
     use frame_support::{dispatch::DispatchResult, inherent::Vec, pallet_prelude::*};
     use frame_system::pallet_prelude::*;
-    
+
     #[pallet::config]
     pub trait Config: frame_system::Config {
         type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
