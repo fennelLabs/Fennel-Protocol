07f80e435b00225d886785ee64f4a50fbba739f8
Andrew Plaza <aplaza@liquidthink.net>
2022-02-09 14:46:37 -0500

    better dependency format

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -44,7 +44,6 @@ pub use pallet_fennel_identity;
 pub use pallet_keystore;
 pub use pallet_signal;
 /// Import the template pallet.
-pub use pallet_template;
 pub use pallet_trust;
 
 /// An index to a block.
@@ -270,11 +269,6 @@ impl pallet_sudo::Config for Runtime {
     type Call = Call;
 }
 
-/// Configure the pallet-template in pallets/template.
-impl pallet_template::Config for Runtime {
-    type Event = Event;
-}
-
 impl pallet_keystore::Config for Runtime {
     type Event = Event;
 }
@@ -308,8 +302,6 @@ construct_runtime!(
         Balances: pallet_balances::{Pallet, Call, Storage, Config<T>, Event<T>},
         TransactionPayment: pallet_transaction_payment::{Pallet, Storage},
         Sudo: pallet_sudo::{Pallet, Call, Config<T>, Storage, Event<T>},
-        // Include the custom logic from the pallet-template in the runtime.
-        TemplateModule: pallet_template::{Pallet, Call, Storage, Event<T>},
         TrustModule: pallet_trust::{Pallet, Call, Storage, Event<T>},
         KeystoreModule: pallet_keystore::{Pallet, Call, Storage, Event<T>},
         SignalModule: pallet_signal::{Pallet, Call, Storage, Event<T>},
@@ -490,7 +482,6 @@ impl_runtime_apis! {
             list_benchmark!(list, extra, frame_system, SystemBench::<Runtime>);
             list_benchmark!(list, extra, pallet_balances, Balances);
             list_benchmark!(list, extra, pallet_timestamp, Timestamp);
-            list_benchmark!(list, extra, pallet_template, TemplateModule);
             list_benchmark!(list, extra, pallet_trust, TrustModule);
             list_benchmark!(list, extra, pallet_fennel_identity, IdentityModule);
 
@@ -526,7 +517,6 @@ impl_runtime_apis! {
             add_benchmark!(params, batches, frame_system, SystemBench::<Runtime>);
             add_benchmark!(params, batches, pallet_balances, Balances);
             add_benchmark!(params, batches, pallet_timestamp, Timestamp);
-            add_benchmark!(params, batches, pallet_template, TemplateModule);
             add_benchmark!(params, batches, pallet_trust, TrustModule);
             add_benchmark!(params, batches, pallet_fennel_identity, IdentityModule);
 
