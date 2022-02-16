840879a3437222e40b347bb2473807c073d1527f
Ed Hertzog <edhertzog@gmail.com>
2022-02-16 15:12:25 -0500

    Added benchmarks for keystore

--- pallets/keystore/src/tests.rs
+++ pallets/keystore/src/tests.rs
@@ -6,6 +6,6 @@ fn announce_key() {
     new_test_ext().execute_with(|| {
         let luke = "Luke".as_bytes().to_vec();
         let skywalker = "Skywalker".as_bytes().to_vec();
-        assert_ok!(SignalModule::announce_key(Origin::signed(1), luke, skywalker));
+        assert_ok!(KeystoreModule::announce_key(Origin::signed(1), luke, skywalker));
     });
 }
