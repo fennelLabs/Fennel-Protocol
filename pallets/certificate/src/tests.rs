cb1beb22cf4b1b5a9db15bdc5b8c41a63f200a29
Sean Batzel <romulus108@protonmail.com>
2023-05-10 15:45:02 -0400

    Added event checks to certificate pallet tests.

--- pallets/certificate/src/tests.rs
+++ pallets/certificate/src/tests.rs
@@ -4,22 +4,29 @@ use frame_support::{assert_noop, assert_ok};
 #[test]
 fn test_send_certificate() {
     new_test_ext().execute_with(|| {
+        System::set_block_number(1);
         assert_ok!(CertificateModule::send_certificate(RuntimeOrigin::signed(1), 1));
+        System::assert_last_event(crate::Event::CertificateSent(1, 1).into());
     });
 }
 
 #[test]
 fn test_revoke_certificate() {
     new_test_ext().execute_with(|| {
+        System::set_block_number(1);
         assert_ok!(CertificateModule::send_certificate(RuntimeOrigin::signed(1), 1));
+        System::assert_last_event(crate::Event::CertificateSent(1, 1).into());
         assert_ok!(CertificateModule::revoke_certificate(RuntimeOrigin::signed(1), 1));
+        System::assert_last_event(crate::Event::CertificateRevoked(1, 1).into());
     });
 }
 
 #[test]
 fn test_try_revoke_unowned_certificate() {
     new_test_ext().execute_with(|| {
+        System::set_block_number(1);
         assert_ok!(CertificateModule::send_certificate(RuntimeOrigin::signed(1), 1));
+        System::assert_last_event(crate::Event::CertificateSent(1, 1).into());
         assert_noop!(
             CertificateModule::revoke_certificate(RuntimeOrigin::signed(2), 1),
             Error::<Test>::CertificateNotOwned
