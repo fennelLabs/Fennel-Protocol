de5fa84290b5189d1edaac469c4b6836f6db4411
Sean Batzel <romulus108@protonmail.com>
2023-05-19 16:42:33 -0400

    Added checks to Trust benchmarks.

--- pallets/trust/src/benchmarking.rs
+++ pallets/trust/src/benchmarking.rs
@@ -11,35 +11,59 @@ benchmarks! {
     issue_trust {
         let target: T::AccountId = whitelisted_caller();
         let caller: T::AccountId = whitelisted_caller();
-    }: _(RawOrigin::Signed(caller), target)
+    }: _(RawOrigin::Signed(caller.clone()), target.clone())
+    verify {
+        assert_eq!(CurrentIssued::<T>::get(), 1);
+        assert!(TrustIssuance::<T>::contains_key(caller.clone(), target.clone()));
+    }
 
     revoke_trust {
         let target: T::AccountId = whitelisted_caller();
         let caller: T::AccountId = whitelisted_caller();
-    }: _(RawOrigin::Signed(caller), target)
+    }: _(RawOrigin::Signed(caller.clone()), target.clone())
+    verify {
+        assert_eq!(CurrentRevoked::<T>::get(), 1);
+        assert!(TrustRevocation::<T>::contains_key(caller.clone(), target.clone()));
+    }
 
     remove_trust {
         let target: T::AccountId = whitelisted_caller();
         let caller: T::AccountId = whitelisted_caller();
         Trust::<T>::issue_trust(RawOrigin::Signed(caller.clone()).into(), target.clone())?;
-    }: _(RawOrigin::Signed(caller), target)
+    }: _(RawOrigin::Signed(caller.clone()), target.clone())
+    verify {
+        assert_eq!(CurrentIssued::<T>::get(), 0);
+        assert!(!TrustIssuance::<T>::contains_key(caller.clone(), target.clone()));
+    }
 
     remove_revoked_trust {
         let target: T::AccountId = whitelisted_caller();
         let caller: T::AccountId = whitelisted_caller();
         Trust::<T>::revoke_trust(RawOrigin::Signed(caller.clone()).into(), target.clone())?;
-    }: _(RawOrigin::Signed(caller), target)
+    }: _(RawOrigin::Signed(caller.clone()), target.clone())
+    verify {
+        assert_eq!(CurrentRevoked::<T>::get(), 0);
+        assert!(!TrustRevocation::<T>::contains_key(caller.clone(), target.clone()));
+    }
 
     request_trust {
         let target: T::AccountId = whitelisted_caller();
         let caller: T::AccountId = whitelisted_caller();
-    }: _(RawOrigin::Signed(caller), target)
+    }: _(RawOrigin::Signed(caller.clone()), target.clone())
+    verify {
+        assert_eq!(CurrentRequests::<T>::get(), 1);
+        assert!(TrustRequestList::<T>::contains_key(caller.clone(), target.clone()));
+    }
 
     cancel_trust_request {
         let target: T::AccountId = whitelisted_caller();
         let caller: T::AccountId = whitelisted_caller();
         Trust::<T>::request_trust(RawOrigin::Signed(caller.clone()).into(), target.clone())?;
-    }: _(RawOrigin::Signed(caller), target)
+    }: _(RawOrigin::Signed(caller.clone()), target.clone())
+    verify {
+        assert_eq!(CurrentRequests::<T>::get(), 0);
+        assert!(!TrustRequestList::<T>::contains_key(caller.clone(), target.clone()));
+    }
 }
 
 impl_benchmark_test_suite!(Trust, crate::mock::new_test_ext(), crate::mock::Test);
