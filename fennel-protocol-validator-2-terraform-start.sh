a794b04f8753f13e8860c10e68d0f06983b6bf72
Sean Batzel <romulus108@protonmail.com>
2023-06-13 20:46:11 -0400

    Triggering a rebuild with new scripts.

--- fennel-protocol-validator-2-terraform-start.sh
+++ fennel-protocol-validator-2-terraform-start.sh
@@ -2,5 +2,6 @@
 sudo apt-get update
 sudo apt-get install -y docker.io
 gcloud auth print-access-token | docker login -u oauth2accesstoken --password-stdin us-east1-docker.pkg.dev
+docker pull us-east1-docker.pkg.dev/whiteflag-0/fennel-docker-registry/polkadot-protocol:latest
 docker run -dit -p 30333:30333 -p 9944:9944 --name fennel-validator-2 us-east1-docker.pkg.dev/whiteflag-0/fennel-docker-registry/polkadot-protocol:latest
 docker exec -it fennel-validator-2 /app/target/release/polkadot --validator --base-path /tmp/relay/charlie --chain /app/chainspec.json --port 30333 --ws-port 9944 --bootnodes /ip4/$(gsutil cat gs://whiteflag-0-admin/fennel-protocol-boot-ip.sh)/tcp/30333/p2p/$(gsutil cat gs://whiteflag-0-admin/fennel-node-key.sh) --telemetry-url "wss://telemetry.polkadot.io/submit/ 0" --rpc-methods Unsafe --name FnlDot1
