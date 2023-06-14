6c180403fc577ba4d9af5dd407fab10c85e15b22
Sean Batzel <romulus108@protonmail.com>
2023-06-14 14:33:50 -0400

    Inserting keys before running.

--- fennel-protocol-boot-terraform-start.sh
+++ fennel-protocol-boot-terraform-start.sh
@@ -12,4 +12,6 @@ sudo systemctl restart nginx
 gcloud auth print-access-token | docker login -u oauth2accesstoken --password-stdin us-east1-docker.pkg.dev
 docker pull us-east1-docker.pkg.dev/whiteflag-0/fennel-docker-registry/polkadot-protocol:latest
 docker run -dt -p 9944:9944 -p 30333:30333 -p 9934:9934 --name relay-boot us-east1-docker.pkg.dev/whiteflag-0/fennel-docker-registry/polkadot-protocol:latest
+docker exec relay-boot /app/target/release/polkadot key insert --base-path /tmp/relay/alice --chain /app/chainspec.json --key-type babe --scheme sr25519 --suri $(gsutil cat gs://whiteflag-0-admin/fennel-boot-key.sh)
+docker exec relay-boot /app/target/release/polkadot key insert --base-path /tmp/relay/alice --chain /app/chainspec.json --key-type grandpa --scheme ed25519 --suri $(gsutil cat gs://whiteflag-0-admin/fennel-boot-key.sh)
 docker exec relay-boot /app/target/release/polkadot --validator --base-path /tmp/relay/alice --chain /app/chainspec.json --port 30333 --ws-port 9944 --rpc-port 9934 --telemetry-url "wss://telemetry.polkadot.io/submit/ 0" --name FnlDot0 --node-key $(gsutil cat gs://whiteflag-0-admin/fennel-node-key.sh) --rpc-cors all --prometheus-external --rpc-methods Unsafe --rpc-external --ws-external
